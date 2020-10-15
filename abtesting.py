d5a53468b5ce4c474e5dca3296ad06fb434d038a
sarahbawabe <sarah_bawabe@brown.edu>
2020-10-14 21:39:29 -0400

    adding python stencil file

---
+++ abtesting.py
@@ -0,0 +1,119 @@
+from scipy import stats
+from scipy.stats import t as t_dist
+from scipy.stats import chi2
+
+# You can comment out these lines! They are just here to help follow along to the tutorial.
+print(t_dist.cdf(-2, 20)) # should print .02963
+print(t_dist.cdf(2, 20)) # positive t-score (bad), should print .97036 (= 1 - .2963)
+
+print(chi2.cdf(23.6, 12)) # prints 0.976
+print(1 - chi2.cdf(23.6, 12)) # prints 1 - 0.976 = 0.023 (yay!)
+
+#TODO: Fill in the following functions! Be sure to delete "pass" when you want to use/run a function!
+
+def get_avg(list):
+    '''
+    Helper function for calculating the average of a list of numbers.
+    :param list: list of numbers
+    :return: average of list
+    '''
+    #TODO: fill me in!
+    pass
+
+def get_stdev(list):
+    '''
+    Helper function for calculating the standard deviation of a list of numbers.
+    :param list: list of numbers
+    :return: standard deviation of list
+    '''
+    #TODO: fill me in!
+    pass
+
+def get_standard_error(a, b):
+    '''
+    Helper function for calculating the standard error, given two lists of numbers.
+    :param a: list of numbers
+    :param b: list of numbers
+    :return: standard error of a and b (see studio 6 guide for this equation!)
+    '''
+    #TODO: fill me in!
+    pass
+
+def get_2_sample_df(a, b):
+    '''
+    Calculates the combined degrees of freedom between two lists of numbers.
+    :param a: list of numbers
+    :param b: list of numbers
+    :return: integer representing the degrees of freedom between a and b (see studio 6 guide for this equation!)
+    HINT: you can use Math.round() to help you round!
+    '''
+    #TODO: fill me in!
+    pass
+
+def t_score(a, b):
+    '''
+    Calculates the t-score, given two lists of numbers.
+    :param a: list of numbers
+    :param b: list of numbers
+    :return: number representing the t-score given lists a and b (see studio 6 guide for this equation!)
+    '''
+    #TODO: fill me in!
+    pass
+
+def perform_2_sample_t_test(a, b):
+    '''
+    ** DO NOT CHANGE THE NAME OF THIS FUNCTION!! ** (this will mess with our autograder)
+    Calculates a p-value by performing a 2-sample t-test, given two lists of numbers.
+    :param a: list of numbers
+    :param b: list of numbers
+    :return: calculated p-value
+    HINT: the t_dist.cdf() function might come in handy!
+    '''
+    #TODO: fill me in!
+    pass
+
+def get_expected_grid(observed_grid):
+    '''
+    Calculates the expected counts, given the observed counts.
+    ** DO NOT modify the parameter, observed_grid. **
+    :param observed_grid: 2D list of observed counts
+    :return: 2D list of expected counts
+    HINT: To clean up this calculation, consider filling in the optional helper functions below!
+    '''
+    #TODO: fill me in!
+    pass
+
+# [OPTIONAL] Some helper functions that might be helpful in get_expected_grid().
+# def row_sum(observed_grid, ele_row):
+# def col_sum(observed_grid, ele_col):
+# def total_sum(observed_grid):
+# def calculate_expected(row_sum, col_sum, tot_sum):
+
+def df_chi2(observed_grid):
+    '''
+    Calculates the degrees of freedom of the expected counts.
+    :param observed_grid: 2D list of observed counts
+    :return: degrees of freedom of expected counts (see studio 6 guide for this equation!)
+    '''
+    #TODO: fill me in!
+    pass
+
+def chi2_value(observed_grid):
+    '''
+    Calculates the chi^2 value of the expected counts.
+    :param observed_grid: 2D list of observed counts
+    :return: associated chi^2 value of expected counts (see studio 6 guide for this equation!)
+    '''
+    #TODO: fill me in!
+    pass
+
+def perform_chi2_homogeneity_test(observed_grid):
+    '''
+    ** DO NOT CHANGE THE NAME OF THIS FUNCTION!! ** (this will mess with our autograder)
+    Calculates the p-value by performing a chi^2 test, given a list of observed counts
+    :param observed_grid: 2D list of observed counts
+    :return: calculated p-value
+    HINT: the chi2.cdf() function might come in handy!
+    '''
+    #TODO: fill me in!
+    pass
